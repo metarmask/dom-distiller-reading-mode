fc4b387219f4e170ff8e1f20e47e2ddbedeba94c
metarmask <ihvarfner@gmail.com>
2016-10-15 12:16:48 +0200

    Adjust root font size

--- src/viewer.js
+++ src/viewer.js
@@ -1,5 +1,6 @@
 /* global addToPage */
+
+// Makes regular text have the regular text size
+document.body.style.fontSize = "1.33333333em";
 const result = JSON.parse(localStorage["result_" + location.hash.substr(1)]);
 addToPage(result[2][1]);
-
-console.log("Hi!");
