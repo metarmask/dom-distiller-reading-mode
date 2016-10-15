e917c010a07c9478987fae12e505286c1d3e6aae
metarmask <ihvarfner@gmail.com>
2016-10-15 10:53:23 +0200

    Split tasks and build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const gulp = require("gulp");
 const replace = require("gulp-replace");
 const del = require("del");
@@ -9,7 +11,7 @@ gulp.task("clean", () => {
     return del(["out"]);
 });
 
-gulp.task("extract", ["clean"], () => {
+gulp.task("extract:distillerCore", ["clean"], () => {
     return gulp.src([
         distillerCore + "/css" + allChildren,
         distillerCore + "/html" + allChildren,
@@ -17,3 +19,27 @@ gulp.task("extract", ["clean"], () => {
         distillerCore + "/javascript" + allChildren
     ], {base: distillerCore}).pipe(gulp.dest("out/dom-distiller"));
 });
+
+gulp.task("extract:internal-src", ["clean"], () => {
+    return gulp.src(["./**/*", "!./external/**/*"], {base: "./src"})
+    .pipe(gulp.dest("./out/"));
+});
+
+gulp.task("extract", ["extract:distillerCore", "extract:internal-src"]);
+
+gulp.task("build:dom_distiller_viewer", ["extract"], () => {
+    const spinner = fs.readFileSync("out/dom-distiller/images/dom_distiller_material_spinner.svg", "utf8");
+    return gulp.src("out/dom-distiller/html/dom_distiller_viewer.html", {base: "./"})
+    .pipe(replace("$6", spinner))
+    .pipe(replace("$2", `<link href="../css/distilledpage.css" rel="stylesheet" type="text/css">`))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("build:domdistiller", ["extract"], () => {
+    const js = fs.readFileSync("src/external/dom-distiller/out/package/js/domdistiller.js", "utf8");
+    return gulp.src("out/dom-distiller/javascript/domdistiller.js", {base: "./"})
+    .pipe(replace(`<include src="../../../../third_party/dom_distiller_js/dist/js/domdistiller.js"/>`, js))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("build", ["build:dom_distiller_viewer", "build:domdistiller"]);
