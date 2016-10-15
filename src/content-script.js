16c5585ec3b9e53aad00c5ad5d82d350d23e6e32
metarmask <ihvarfner@gmail.com>
2016-10-15 12:16:19 +0200

    Set iframe styles more easily

--- src/content-script.js
+++ src/content-script.js
@@ -6,16 +6,21 @@ if(existingIframe) {
 } else {
     const iframe = document.createElement("iframe");
     iframe.src = chrome.runtime.getURL("dom-distiller/html/dom_distiller_viewer.html") + "#" + tabID;
-    iframe.style.zIndex = 100000000;
-    iframe.style.position = "fixed";
-    iframe.style.left = 0;
-    iframe.style.right = 0;
-    iframe.style.top = 0;
-    iframe.style.bottom = 0;
-    iframe.style.margin = "auto";
-    iframe.style.width = "100vw";
-    iframe.style.height = "100vh";
-    iframe.style.background = "white";
-    iframe.style.border = "none";
+    const style = {
+        zIndex: 100000000,
+        position: "fixed",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        margin: "auto",
+        width: "100vw",
+        height: "100vh",
+        background: "white",
+        border: "none"
+    };
+    Object.keys(style).forEach(property => {
+        iframe.style[property] = style[property];
+    });
     document.documentElement.appendChild(iframe);
 }
