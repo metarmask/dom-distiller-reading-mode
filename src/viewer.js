992efc14fb22b55a70076cbb80e757e71780b877
metarmask <ihvarfner@gmail.com>
2016-10-24 12:10:00 +0200

    Handle themes and fonts in viewer

--- src/viewer.js
+++ src/viewer.js
@@ -4,6 +4,8 @@
 // Makes regular text have the regular text size
 document.body.style.fontSize = "1.33333333em";
 
+document.body.className = "light sans-serif";
+
 const oldSetTitle = setTitle;
 setTitle = () => {
     window.top.postMessage({action: "setTitle", title: arguments[0]}, "*");
@@ -13,3 +15,8 @@ setTitle = () => {
 const result = JSON.parse(localStorage["result_" + location.hash.substr(1)]);
 addToPage(result[2][1]);
 setTitle(result[1]);
+
+chrome.storage.sync.get({theme: "light", font: "sans-serif"}, ({theme, font}) => {
+	useTheme(theme);
+	useFontFamily(font);
+});
