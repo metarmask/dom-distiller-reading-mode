71a2d98cbf2305d1b4919cb4db90bf68285cb662
metarmask <ihvarfner@gmail.com>
2016-10-24 18:31:38 +0200

    Move options JS into seperate file

---
+++ src/options/options.js
@@ -0,0 +1,22 @@
+"use strict";
+const groupValues = {};
+
+function updateClassName() {
+	document.body.className = `${groupValues.theme} ${groupValues.font}`;
+}
+
+["theme", "font"].forEach(selectGroup => {
+	document.querySelector("#select-button-group-" + selectGroup)
+	.addEventListener("click", function({target}) {
+		if(!target.classList.contains("select-button")) {
+			return;
+		}
+		const value = target.id.substr(`select-button-${selectGroup}-`.length);
+		groupValues[selectGroup] = value;
+		this.querySelector(".select-button-selected").classList.remove("select-button-selected");
+		target.classList.add("select-button-selected");
+		updateClassName();
+	});
+});
+
+document.body.style.fontSize = "1.33333333em";
