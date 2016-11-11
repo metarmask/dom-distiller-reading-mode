c5fac06057c19ad8830ce99b9fc7bf9e55e69074
metarmask <ihvarfner@gmail.com>
2016-11-11 18:17:45 +0100

    Use i18n API and refactor options

--- src/options/options.js
+++ src/options/options.js
@@ -17,31 +17,42 @@ function parseHTML(html) {
 	return fragment;
 }
 
-const headings = {};
-Array.from(document.querySelectorAll("h2"))
-.forEach(h2 => headings[h2.textContent.toLowerCase()] = h2);
-headings.themes.parentElement.insertBefore(parseHTML("" +
-`<p class="select-button-group" id="select-button-group-theme">
-	<button class="select-button select-button-theme light select-button-selected" id="select-button-theme-light" title="Light"></button>
-	<button class="select-button select-button-theme dark" id="select-button-theme-dark" title="Dark"></button>
-	<button class="select-button select-button-theme sepia" id="select-button-theme-sepia" title="Sepia"></button>
-</p>`), headings.themes.nextElementSibling);
-headings.fonts.parentElement.insertBefore(parseHTML("" +
-`<p class="select-button-group" id="select-button-group-font">
-	<button class="select-button select-button-font sans-serif select-button-selected" id="select-button-font-sans-serif" title="Sans serif">Sa</button>
-	<button class="select-button select-button-font serif" id="select-button-font-serif" title="Serif">Se</button>
-	<button class="select-button select-button-font monospace" id="select-button-font-monospace" title="Monospace">Mo</button>
-</p>`), headings.fonts.nextElementSibling);
+const options = {
+	theme: [
+		"light",
+		"dark",
+		"sepia"
+	],
+	font: [
+		"sans-serif",
+		"serif",
+		"monospace"
+	]
+};
 
-["theme", "font"].forEach(selectGroup => {
-	document.querySelector(`#select-button-group-${selectGroup}`)
-	.addEventListener("click", function({target}) {
-		if(!target.classList.contains("select-button")) {
-			return;
+const headings = document.querySelectorAll("h2");
+Object.keys(options).forEach((option, index) => {
+	const p = document.createElement("p");
+	p.className = "select-button-group";
+	options[option].forEach(value => {
+		const button = document.createElement("button");
+		button.className = `select-button ${value}`;
+		button.title = chrome.i18n.getMessage(
+			`options_${option}_name_${value.replace(/-/g, "_")}`
+		);
+		if(option === "font") {
+			button.textContent = chrome.i18n.getMessage(
+				`options_font_abbreviation_${value.replace(/-/g, "_")}`
+			);
 		}
-		const value = target.id.substr(`select-button-${selectGroup}-`.length);
-		this.querySelector(".select-button-selected").classList.remove("select-button-selected");
-		target.classList.add("select-button-selected");
-		chrome.storage.sync.set({[selectGroup]: value});
+		button.addEventListener("click", () => {
+			chrome.storage.sync.set({[option]: value});
+		});
+		p.appendChild(button);
 	});
+
+	headings[index].parentElement.insertBefore(
+		p,
+		headings[index].nextElementSibling
+	);
 });
--- src/options/preOptions.js
+++ src/options/preOptions.js
@@ -1,3 +1,23 @@
+const msgExpressionStart = "__MSG_";
+const msgExpressionEnd = "__";
+const msgXPathResult = document.evaluate(
+	`//text()[
+		starts-with(., "${msgExpressionStart}") and
+		(substring(., string-length(.) - string-length("${msgExpressionEnd}") + 1) = "${msgExpressionEnd}")
+	]`,
+	document,
+	null,
+	XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE
+);
+
+for (let i = 0; i < msgXPathResult.snapshotLength; i++) {
+	const textNode = msgXPathResult.snapshotItem(i);
+	const messageName = Array.from(textNode.nodeValue).slice(
+		msgExpressionStart.length,
+		-msgExpressionEnd.length
+	).join("");
+	textNode.nodeValue = chrome.i18n.getMessage(messageName);
+}
 
 function onOptionsResultLoaded() {
 	const contentScript = document.createElement("script");
